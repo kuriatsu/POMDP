89219f27ab75f9b2106cc5d388a828ae69b4899f
kuriatsu <kuriatsubayashi712@gmail.com>
2022-12-07 02:14:11 +0900

    operator model show based on yaml

--- operator_model.py
+++ operator_model.py
@@ -4,6 +4,7 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import yaml
 
 class OperatorModel:
     def __init__(self, min_time, min_time_var, acc_time_min, acc_time_var, acc_time_slope, int_time_list):
@@ -65,36 +66,50 @@ class OperatorModel:
 
     
 if __name__=="__main__":
-    min_time = 3
-    min_time_var = 0.5
-    acc_time_min = 0.5
-    acc_time_var = 0.1
-    acc_time_slope = 0.2
+
+    # min_time = 3
+    # min_time_var = 0.5
+    # acc_time_min = 0.5
+    # acc_time_var = 0.1
+    # acc_time_slope = 0.2
     int_time_list = [0, 1, 2, 3, 4, 5, 6]
-    operator_model = OperatorModel(
-        min_time,
-        min_time_var,
-        acc_time_min,
-        acc_time_var,
-        acc_time_slope,
-        int_time_list,
-        )
-    acc = operator_model.get_acc_prob(4)
-    print(acc)
-    operator_model.acc_list = np.linspace(0.1, 1.0, 10).tolist()
-    operator_model.init_performance_model()
-    
-    acc_mat = np.zeros((len(int_time_list), len(operator_model.acc_list)))
-    for int_time in int_time_list:
-        acc_prob_list = operator_model.get_acc_prob(int_time)
-        for acc_prob in acc_prob_list:
-            print(acc_prob)
-            if acc_prob[0] is None:
-                acc_mat[int_time,0] = acc_prob[1]
-            else:
-                acc_mat[int_time, int(10*acc_prob[0]-1)] = acc_prob[1]
-    sns.heatmap(np.rot90(acc_mat))
-    plt.yticks([i for i in range(10)], [round(i, 1) for i in np.linspace(1.0, 0.1, 10).tolist()])
-    plt.xlabel("intervention time [s]", fontsize=14)
-    plt.ylabel("intervention accuracy", fontsize=14)
+    param_list = [
+            "param_2_1.yaml",
+            # "param_3_1.yaml",
+            # "param_4_1.yaml",
+            "param_6_1.yaml",
+            "param_7_1.yaml",
+            ]
+    fig, axes = plt.subplots(1, len(param_list))
+    for i, param in enumerate(param_list):
+
+        with open(param) as f:
+            param = yaml.safe_load(f)
+
+        operator_model = OperatorModel(
+            param["min_time"],
+            param["min_time_var"],
+            param["acc_time_min"],
+            param["acc_time_var"],
+            param["acc_time_slope"],
+            int_time_list,
+            )
+        acc = operator_model.get_acc_prob(4)
+        print(acc)
+        operator_model.acc_list = np.arange(0.0, 1.25, 0.25).tolist()
+        operator_model.init_performance_model()
+        
+        acc_mat = np.zeros((len(int_time_list), len(operator_model.acc_list)))
+        for int_time in int_time_list:
+            acc_prob_list = operator_model.get_acc_prob(int_time)
+            for acc_prob in acc_prob_list:
+                # print(acc_prob)
+                if acc_prob[0] is None:
+                    acc_mat[int_time,0] = acc_prob[1]
+                else:
+                    acc_mat[int_time, int((acc_prob[0]/0.25))] = acc_prob[1]
+        sns.heatmap(np.rot90(acc_mat), ax=axes[i])
+        axes[i].set_yticklabels([round(i, 2) for i in np.arange(1.0, -0.25, -0.25).tolist()])
+        axes[i].set_xlabel("intervention time [s]", fontsize=14)
+        axes[i].set_ylabel("intervention accuracy", fontsize=14)
     plt.show()
