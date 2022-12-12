e5c4ec1302920d8e8fb69055a9de8554044867f2
kuriatsu <kuribayashi.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2022-12-12 18:37:12 +0900

    add params 3 obj

--- run_sweep_multi.sh
+++ run_sweep_multi.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-python3 ras_value_iteration_sweep.py param_12_1.yaml &
-python3 ras_value_iteration_sweep.py param_12_2.yaml &
-python3 ras_value_iteration_sweep.py param_12_3.yaml &
-python3 ras_value_iteration_sweep.py param_12_4.yaml &
-python3 ras_value_iteration_sweep.py param_12_5.yaml &
-wait
-python3 ras_value_iteration_sweep.py param_10_1.yaml &
-python3 ras_value_iteration_sweep.py param_10_2.yaml &
-python3 ras_value_iteration_sweep.py param_10_3.yaml &
-python3 ras_value_iteration_sweep.py param_10_4.yaml &
-wait
-python3 ras_value_iteration_sweep.py param_11_1.yaml &
-python3 ras_value_iteration_sweep.py param_11_2.yaml &
+python3 ras_value_iteration_sweep.py param_1_3obj.yaml &
+python3 ras_value_iteration_sweep.py param_2_3obj.yaml &
+python3 ras_value_iteration_sweep.py param_3_3obj.yaml &
+python3 ras_value_iteration_sweep.py param_4_3obj.yaml &
+python3 ras_value_iteration_sweep.py param_5_3obj.yaml &
+python3 ras_value_iteration_sweep.py param_6_3obj.yaml &
+python3 ras_value_iteration_sweep.py param_7_3obj.yaml &
